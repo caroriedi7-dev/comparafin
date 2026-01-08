9421277c8405f68f1f5ef1a0458010b3f338823f
caroriedi7-dev <caro.riedi7@gmail.com>
2026-01-08 16:08:27 +0100

    Delete app directory

--- app/contas/page.js
+++
@@ -1,8 +0,0 @@
-export default function Contas() {
-  return (
-    <section>
-      <h1>Comparar contas bancárias</h1>
-      <p>Página de teste funcionando.</p>
-    </section>
-  );
-}
--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <section>
-      <h1>Comparafin</h1>
-      <p>Comparador de contas e cartões bancários no Brasil.</p>
-    </section>
-  );
-}
